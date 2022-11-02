0a3a5df36c7cab44d7d6392286413518d64856b9
Willi0t <william.sinclair92@gmail.com>
2022-11-02 16:34:24 +0100

    change to scandinavia script

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-const videoclip = document.querySelectorAll('.videoclip');
-for (let i = 0; i<videoclip.length; i++){
-    videoclip[i].addEventListener('mouseenter', function(e){
-        videoclip[i].play()
-    })
-    videoclip[i].addEventListener('mouseout', function(e){
-        videoclip[i].pause()
-    })
+const videoclip = document.querySelectorAll(".videoclip");
+for (let i = 0; i < videoclip.length; i++) {
+  videoclip[i].addEventListener("mouseenter", function (e) {
+    videoclip[i].play();
+  });
+  videoclip[i].addEventListener("mouseout", function (e) {
+    videoclip[i].pause();
+  });
 }
 
 const myfunction = () => {
-    toggle=!toggle;
-    if(toggle===true){
-        document.getElementById('explore2').style.left='0';
-        
-    }
-    else {
-        document.getElementById('explore2').style.left='-100%';
-    }
-}
+  toggle = !toggle;
+  if (toggle === true) {
+    document.getElementById("explore2").style.left = "0";
+  } else {
+    document.getElementById("explore2").style.left = "-100%";
+  }
+};
 let toggle = false;
